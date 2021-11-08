b3b262b2795bfdca46d20b56fd601357676f3602
hhnguyen91 <47880450+hhnguyen91@users.noreply.github.com>
2021-11-08 13:44:36 -0800

    Added assignment table to database

--- app/src/main/java/com/example/gradecheckhhn/database/AppDatabase.kt
+++ app/src/main/java/com/example/gradecheckhhn/database/AppDatabase.kt
@@ -3,12 +3,13 @@ package com.example.gradecheckhhn.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.gradecheckhhn.databaseEntities.Assignment
 import com.example.gradecheckhhn.databaseEntities.Course
 import com.example.gradecheckhhn.databaseEntities.Semester
 
 private const val DATABASE_NAME = "semester-database"
 
-@Database(entities = [ Semester::class, Course::class ], version = 1)
+@Database(entities = [ Semester::class, Course::class , Assignment::class], version = 1)
 @TypeConverters(DBTypeConverters::class)
 abstract class AppDatabase : RoomDatabase() {
 
